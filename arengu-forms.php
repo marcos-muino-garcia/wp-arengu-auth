e3813ab80bf65f9a94068c6a29a1da0794643cb6
Martín Melado <217925+martinml@users.noreply.github.com>
2021-10-14 12:08:32 +0200

    Add comment explaining 3cb8885

--- arengu-forms.php
+++ arengu-forms.php
@@ -1,1 +1,15 @@
-<?php require('arengu-auth.php');
+<?php
+
+/**
+ * The main file in older v2 versions of this plugin was called
+ * `arengu-forms.php`, but with v3 it was renamed to `arengu-auth.php`
+ * to better reflect its nature.
+ *
+ * Turns out, if you had v2 installed, WordPress remembers the path and
+ * tries to load the old file, which obviously fails.
+ *
+ * Fix it creating this dummy file that only loads the new renamed file.
+ */
+
+require('arengu-auth.php');
+
